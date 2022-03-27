25e4f93ac74660fbf9ce534181d4604cd4714ff5
Краснов Андрей Асхатович <a.krasnov@owen.ru>
2022-03-27 11:02:22 +0300

    Initial commit

--- src/Loader.java
+++ src/Loader.java
@@ -6,6 +6,10 @@ public class Loader
         Cat murka = new Cat();
         Cat barsik = new Cat();
         Cat vaska = new Cat();
+        Cat murzik = new Cat();
+        Cat mushka = new Cat();
+        Cat busya = new Cat();
+
 
         System.out.println("Вес Мурки: " + murka.getWeight());
         murka.feed(murka.getWeight() / 10);
@@ -15,6 +19,20 @@ public class Loader
         murka.poop();
         System.out.println("Вес Мурки после тулета стал: " + murka.getWeight());
         System.out.println(murka.getStatus());
+
+        System.out.println("Начальный вес Барсика: " + barsik.getWeight());
+        while (barsik.getStatus() != "Dead") {
+            barsik.meow();
+            System.out.println("Текущий вес Барсика: " + barsik.getWeight());
+            System.out.println(barsik.getStatus());
+        }
+
+        System.out.println("Начальный вес Васьки: " + vaska.getWeight());
+        while (vaska.getStatus() != "Exploded") {
+            vaska.drink(vaska.getWeight() / 100);
+            System.out.println("Васька попил и его стал: " + vaska.getWeight());
+            System.out.println(vaska.getStatus());
+        }
 
     }
 }
